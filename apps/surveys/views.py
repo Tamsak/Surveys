691fe7308a6b3769fe33f86bca7911e6964803f8
Tamsak <35890300+Tamsak@users.noreply.github.com>
2018-04-11 01:01:58 -0700

    Update views.py

--- apps/surveys/views.py
+++ apps/surveys/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
+    if 'counter' not in request.session:
+    request.session['counter'] = 0
     return render(request, 'surveys/index.html')
 def submit(request):
     if request.method == 'POST':
@@ -12,6 +14,7 @@ def submit(request):
         request.session['comment'] = request.POST['comment'] 
     return redirect('process/result')
 def result(request):
+    request.session['counter'] +=1
     info = {
             'name': request.session['name'], 
             'location' :request.session['location'],
